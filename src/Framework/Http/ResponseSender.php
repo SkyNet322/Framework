0d2cf402972c20f7917148ddbaa3e098d8723e6d
SkyNet322 <aliev_arkadij@mail.ru>
2019-06-21 15:49:15 +0300

    start

---
+++ src/Framework/Http/ResponseSender.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilix
+ * Date: 6/21/19
+ * Time: 1:47 PM
+ */
+
+namespace Framework\Http;
+
+use Psr\Http\Message\ResponseInterface;
+
+class ResponseSender
+{
+    public function send(ResponseInterface $response): void
+    {
+        header(sprintf(
+           'HTTP/%s $d $s',
+            $response->getProtocolVersion(),
+            $response->getStatusCode(),
+            $response->getReasonPhrase()
+        ));
+        foreach ($response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header(sprintf('$s: $s', $name, $value), false);
+            }
+        }
+        echo $response->getBody()->getContents();
+    }
+
+}
